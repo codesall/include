94bb9bdd8ab9a71f1103a5d0f8a5c1571b290d9e
codesall <153760649+codesall@users.noreply.github.com>
2025-05-01 00:03:46 +0530

    Delete bin directory

--- bin/BasicScalaPos.scala
+++
@@ -1,8 +0,0 @@
-object HelloWorld {
-	def main(args: Array[String]): Unit = {
-        val number = scala.io.StdIn.readInt()
-        if (number > 0) println(s"$number is Positive")
-        else if (number < 0) println(s"$number is Negative")
-        else println(s"$number is Zero")
-    }
-}
